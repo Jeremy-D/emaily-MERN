bd14af9aa88a8dfbf255766fc882cb4466feafbb
Jeremy DeCarvalho <jeremypdecarvalho@gmail.com>
2019-05-27 15:11:24 -0700

    91 add routing logic in navbar that checks if user is logged in

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,20 +1,41 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
+	renderContent(){
+		switch(this.props.auth){
+			case null:
+				return;
+			case false:
+				return (
+					<li><a href="/auth/google">Login With Google</a></li>
+				);
+			default:
+				return <li><a href="/api/logout">Logout</a></li>
+		}
+	}
+
 	render() {
 		return (
-			 <nav>
-			    <div href="www.example.com" className="nav-wrapper">
-			      <a className="brand-logo">Emaily</a>
-			      <ul id="nav-mobile" className="right hide-on-med-and-down">
-			        <li><a href="sass.html">Login With Google</a></li>
-			        <li><a href="badges.html">Components</a></li>
-			        <li><a href="collapsible.html">JavaScript</a></li>
-			      </ul>
-			    </div>
-			  </nav>
+			<nav>
+				<div className="nav-wrapper">
+					<Link
+						to={this.props.auth ? '/surveys' : '/'} 
+						className="left brand-logo"
+					>
+						Emaily
+					</Link>
+					<ul className="right">
+						{ this.renderContent() }
+					</ul>
+				</div>    
+			</nav>
 		)
 	}
 }
 
-export default Header;
+function mapStateToProps(state){
+	return { auth: state.auth };
+}
+export default connect(mapStateToProps)(Header);

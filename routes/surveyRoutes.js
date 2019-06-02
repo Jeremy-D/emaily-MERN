1822cee7de98e6d74447c808359042daeb428c04
Jeremy DeCarvalho <jeremypdecarvalho@gmail.com>
2019-06-01 18:17:41 -0700

    138 complete sendgrid setup

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate.js');
 
 const Survey = mongoose.model('surveys');
 module.exports = app =>{
@@ -17,6 +19,9 @@ module.exports = app =>{
 			dateSent: Date.now()
 
 		})
+
+		//Send email here
+		const mailer = new Mailer(survey, surveyTemplate(survey))
 
 		survey.save()
 	});

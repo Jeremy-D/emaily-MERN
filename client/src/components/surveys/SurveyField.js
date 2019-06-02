b3ffbb999a71fa03f0e71c89cedff222eef56d93
Jeremy DeCarvalho <jeremypdecarvalho@gmail.com>
2019-06-02 14:29:57 -0700

    162 form validation with redux-form

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -2,12 +2,16 @@
 //label and text input
 import React from 'react'
 
-export default ({input, label}) => {
+export default ({input, label, meta: {error, touched}}) => {
 //{input} is es6 destructuring for props.input
+
 	return (
 		<div>
 			<label>{label}</label>
-			<input {...input}/>
+			<input {...input} style={{marginBottom:'5px'}}/>
+			<div className="red-text" style={{marginBottom:'20px'}}>
+				{touched && error}
+			</div>
 		</div>
 	);
 };
--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -3,6 +3,8 @@ import _ from 'lodash';
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import SurveyField from './SurveyField';
+import { Link } from 'react-router-dom';
+import validateEmails from '../../utils/validateEmails';
 
 const FIELDS = [
 	{ label: 'Survey Title', name:'title'},
@@ -15,7 +17,7 @@ class SurveyForm extends Component{
 	renderFields(){
 		return (
 			_.map(FIELDS, ({label, name}) => {
-				return <Field component={SurveyField} type="text" label={label} name={name} />
+				return <Field key={name} component={SurveyField} type="text" label={label} name={name} />
 			})
 		);
 	};
@@ -25,13 +27,38 @@ class SurveyForm extends Component{
 			<div>
 				<form onSubmit={this.props.handleSubmit(values => console.log(values))}>
 					{this.renderFields()}
-					<button type="submit">Submit</button>
+					<Link to="/surveys" className="red btn-flat white-text">
+						Cancel
+					</Link>
+					<button type="submit" className="teal btn-flat right white-text">
+						Next
+						<i className="material-icons right">done</i>
+					</button>
 				</form>
 			</div>
 		)
 	}
 }
 
+function validate(values){
+	const errors = {}
+	
+	errors.emails = validateEmails(values.emails || '');
+
+	_.each(FIELDS, ({name})=>{
+		if(!values[name]){
+			errors[name] = 'You must provide a value for this field.';
+		}
+	});
+
+
+
+
+	return errors;
+}
+
 export default reduxForm({
+	validate: validate,
 	form: 'surveyForm'
 })(SurveyForm);
+

52ec6dc64bd64538cf17bfac61662e47b1e8e7fa
Jeremy DeCarvalho <jeremypdecarvalho@gmail.com>
2019-06-05 20:05:02 -0700

    add redux flow for connecting react app with mongo backend

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -9,7 +9,15 @@ const Mailer = require('../services/Mailer');
 const surveyTemplate = require('../services/emailTemplates/surveyTemplate.js');
 
 const Survey = mongoose.model('surveys');
+
 module.exports = app =>{
+	app.get('/api/surveys', requireLogin, async (req, res)=>{
+		const surveys = await Survey.find({ _user: req.user.id })
+			.select({ recipients: false });
+
+		res.send(surveys);
+	})
+
 	app.get('/api/surveys/:surveyId/:choice', (req, res)=>{
 		res.send('Thanks for voting!')
 	})

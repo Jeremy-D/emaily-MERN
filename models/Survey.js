93a587c1d26fe4eeeff6a9475fbb739b3f1d764f
Jeremy DeCarvalho <jeremypdecarvalho@gmail.com>
2019-06-05 19:04:56 -0700

    handle email click data with sendgrid webhooks, express, mongo

--- models/Survey.js
+++ models/Survey.js
@@ -5,7 +5,7 @@ const recipientSchema = require('./Recipient');
 const surveySchema = new Schema({
 	title: String,
 	subject: String,
-	subject: String,
+	body: String,
 	recipients: [recipientSchema],
 	yes: {type:Number, default: 0},
 	no: {type:Number, default: 0},
